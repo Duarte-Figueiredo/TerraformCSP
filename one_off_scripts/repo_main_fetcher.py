c230642724271685feb65432a9b3c9267628f438
Duarte Figueiredo <duarte.figueiredo@outlook.com>
2024-04-20 12:22:00 +0100

    Upgraded script responsible for finding main.tf in a repo, so that it fallsback to bfs into a repo if the github repo search fails

--- one_off_scripts/repo_main_fetcher.py
+++ one_off_scripts/repo_main_fetcher.py
@@ -1,21 +1,68 @@
 import asyncio
 import logging
-from typing import List
+from typing import List, Union
 
 from beanie.odm.operators.update.general import Set
+from github import Repository
 from github.ContentFile import ContentFile
 from github.PaginatedList import PaginatedList
 
 from one_off_scripts import initialize_db, GithubSearchResult, DRY_RUN
 from terraform_analyzer.external import github_client
+from terraform_analyzer.external.github_manager import GithubFileType
 
 logger = logging.getLogger("mongo_git_fetcher")
 logging.basicConfig(level=logging.INFO)
 
+FILE_SEARCH_NAME = "main.tf"
+PAGE_SIZE = 50
 
-def find_github_main_root_tf(github_search_result: GithubSearchResult) -> List[ContentFile]:
-    repo_name = github_search_result.id
-    search_query = f"filename:main.tf repo:{repo_name}"
+QUERY = {'$or': [{'downloaded': False}, {'downloaded': {'$exists': False}}]}
+
+
+# QUERY = {'main_tf': {'$exists': False}}
+
+
+def find_github_main_root_tf_bfs(repo_name: str) -> List[ContentFile]:
+    logger.info(f"Breath first search '{FILE_SEARCH_NAME}' in '{repo_name}'")
+
+    repo: Repository
+    try:
+        repo = github_client.get_repo(repo_name)
+    except Exception as e:
+        logger.error(f"Failed to fetch {repo_name}", exc_info=e)
+        return []
+
+    root_contents = repo.get_contents("/")
+
+    folder_to_visit: [ContentFile] = root_contents if type(root_contents) is list else [root_contents]
+
+    main_tf_found: bool = False
+    mains: [ContentFile] = []
+
+    while folder_to_visit:
+        content: ContentFile = folder_to_visit.pop(0)
+        logger.debug(f"Visiting {content.path}/{content.name}")
+
+        if content.type == GithubFileType.DIR.value:
+            if not main_tf_found:
+                tmp: Union[list[ContentFile], ContentFile] = repo.get_contents(content.path)
+                if type(tmp) is list:
+                    folder_to_visit.extend(tmp)
+                else:
+                    folder_to_visit.append(tmp)
+        elif content.type == GithubFileType.FILE.value:
+            if content.name == FILE_SEARCH_NAME:
+                mains.append(content)
+                main_tf_found = True
+        else:
+            raise RuntimeError(f"Unrecognized github file type '{content.type}'")
+
+    return mains
+
+
+def find_github_main_root_tf(repo_name: str) -> List[ContentFile]:
+    search_query = f"filename:{FILE_SEARCH_NAME} repo:{repo_name}"
 
     files: PaginatedList[ContentFile] = github_client.search_code(
         query=search_query
@@ -24,44 +71,50 @@ def find_github_main_root_tf(github_search_result: GithubSearchResult) -> List[C
     mains: [ContentFile] = []
 
     if files.totalCount == 0:
-        logger.info(f"{repo_name}:\tnothing")
-        return mains
+        return find_github_main_root_tf_bfs(repo_name)
 
     for file in files:
         mains.append(file)
 
-    logger.info(f"{repo_name}\t:extracted {len(mains)} main.tf files")
-
     return mains
 
 
-async def fetch_repo(github_search_result: GithubSearchResult):
-    logger.info(f"{github_search_result.id}\t--------")
+def fetch_repo_mains(repo: str) -> [str]:
+    main_files: List[ContentFile] = find_github_main_root_tf(repo)
 
-    main_files: List[ContentFile] = find_github_main_root_tf(github_search_result)
+    logger.info(f"{repo}\t:extracted {len(main_files)} main.tf files")
 
     main_paths: [str] = []
     for content_file in main_files:
         main_paths.append(content_file.path)
 
-    if not DRY_RUN:
-        await github_search_result.update(Set({GithubSearchResult.main_tf: main_paths}))
-
-
-# query = {'$or': [{'downloaded': False}, {'downloaded': {'$exists': False}}]}
-query = {'main_tf': {'$exists': False}}
+    return main_paths
 
 
 async def main():
     await initialize_db()
 
+    index = 0
+
     while True:
-        results = await GithubSearchResult.find(query) \
-            .limit(10) \
+        results: [GithubSearchResult] = await GithubSearchResult.find(QUERY) \
+            .skip(index) \
+            .limit(PAGE_SIZE) \
             .to_list()
 
         for result in results:
-            await fetch_repo(result)
+            logger.info(f"{result.id}")
+
+            mains = fetch_repo_mains(result.id)
+
+            if not DRY_RUN:
+                await result.update(Set({GithubSearchResult.main_tf: mains}))
+
+            logger.info(f"{result.id}:{mains}")
+
+        if not results:
+            break
+        index += PAGE_SIZE
 
 
 if __name__ == '__main__':
